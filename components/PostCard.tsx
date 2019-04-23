38792758080096aeca03718c9b68abf28cbef1db
Shaik Khaja Mohiddin <shaik_k@worksap.co.jp>
2019-04-23 18:57:15 +0900

    separate postcard

---
+++ components/PostCard.tsx
@@ -0,0 +1,114 @@
+import Link from 'next/link';
+import React from 'react';
+
+import {
+  Button, Card, CardActions, CardContent, createStyles, Theme, Typography, withStyles, WithStyles
+} from '@material-ui/core';
+
+const styles = (theme: Theme) => createStyles({
+    item: {
+        width: "300px",
+        height: "450px",
+        padding: theme.spacing.unit,
+        margin: theme.spacing.unit,
+        display: "flex",
+        flexDirection: "column"
+    },
+    itemHeader: {
+        height: "80px",
+        padding: `${theme.spacing.unit}px ${2 * theme.spacing.unit}px`
+    },
+    itemContent: {
+        height: "240px",
+        display: "flex",
+        flexDirection: "column",
+        padding: `${theme.spacing.unit}px ${2 * theme.spacing.unit}px`
+    },
+    itemActions: {
+        flex: 1
+    },
+    itemAuthor: {
+        height: "24px",
+        display: "flex",
+        padding: `0px ${2 * theme.spacing.unit}px`
+    },
+    itemAuthorName: {
+        flex: 1
+    },
+    itemAuthorHandle: {
+        flex: 1
+    },
+    itemsTags: {
+        height: "48px",
+        padding: `0px ${2 * theme.spacing.unit}px`
+    }
+})
+
+type StyleProps = WithStyles<typeof styles>
+
+interface ComponentProps {
+    data: {
+        id: string
+        slug: string
+        title: string
+        author: {
+            name: string | null
+            handle: string
+        }
+        content: string | null
+        tags: Array<{ tag: string }>
+    }
+}
+
+type Props = StyleProps & ComponentProps;
+
+interface State {
+    raised: boolean
+}
+
+class PostCard extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = { raised: false }
+    }
+    render() {
+        const { classes } = this.props;
+        return (
+            <Card className={this.props.classes.item} draggable={true} key={this.props.data.id}
+                onMouseEnter={() => this.setState({ raised: true })} onMouseLeave={() => this.setState({ raised: false })}
+                raised={this.state.raised}>
+                <div className={classes.itemHeader}>
+                    <Link href={"/post?slug=" + this.props.data.slug} as={"/post/" + this.props.data.slug}>
+                        <Typography variant="h6">
+                            {this.props.data.title.substr(0, 40)}
+                        </Typography>
+                    </Link>
+                </div>
+                <div className={this.props.classes.itemAuthor}>
+                    <Typography color="secondary" className={this.props.classes.itemAuthorName}>
+                        {this.props.data.author.name ? this.props.data.author.name.substr(0, 15) : ''}
+                    </Typography>
+                    <Typography color="textSecondary" className={this.props.classes.itemAuthorHandle}>
+                        @{this.props.data.author.handle.substr(0, 15)}
+                    </Typography>
+                </div>
+                <CardContent className={classes.itemContent}>
+                    <Typography variant="body1" >
+                        {this.props.data.content ? this.props.data.content.substr(0, 200) : ''}
+                    </Typography>
+                </CardContent>
+                <div className={classes.itemsTags}>
+                    <Typography variant="subtitle2" color="textSecondary">
+                        {this.props.data.tags.map(t => t.tag).join(", ")}
+                    </Typography>
+                </div>
+                <CardActions className={classes.itemActions}>
+                    <Button>Like</Button>
+                    <Button>Save</Button>
+                </CardActions>
+            </Card>
+        )
+    }
+}
+
+export default withStyles(styles)(PostCard)
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,15 @@ import gql from 'graphql-tag';
 import { debounce } from 'lodash';
 import Pagination from 'material-ui-flat-pagination';
 import { GetInitialProps, NextContext } from 'next';
-import Link from 'next/link';
 import Router from 'next/router';
 import React from 'react';
 import { ApolloConsumer, Query } from 'react-apollo';
 
-import {
-  Card, Chip, createStyles, Divider, NoSsr, Theme, Typography, WithStyles, withStyles
-} from '@material-ui/core';
+import { createStyles, NoSsr, WithStyles, withStyles } from '@material-ui/core';
 
 import Layout from '../components/Layout';
 import MaterialReactSelect from '../components/MaterialReactSelect';
+import PostCard from '../components/PostCard';
 import { GetPostsWithTags, GetPostsWithTagsVariables } from '../types/GetPostsWithTags';
 import { GetTagSuggestions, GetTagSuggestionsVariables } from '../types/GetTagSuggestions';
 
@@ -53,41 +51,11 @@ query GetTagSuggestions($query: String!){
 }
 `
 
-const styles = (theme: Theme) => createStyles({
+const styles = () => createStyles({
   itemContainer: {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "space-evenly"
-  },
-  item: {
-    width: "300px",
-    height: "450px",
-    padding: 2 * theme.spacing.unit,
-    margin: theme.spacing.unit,
-    display: "flex",
-    flexDirection: "column"
-  },
-  itemTitle: {
-    flexBasis: "20%"
-  },
-  itemAuthor: {
-    flexBasis: "10%",
-    display: "flex",
-  },
-  itemAuthorName: {
-    flex: 1
-  },
-  itemAuthorHandle: {
-    flex: 1
-  },
-  itemContent: {
-    flexBasis: "50%"
-  },
-  itemTags: {
-    flexBasis: "10%"
-  },
-  itemActions: {
-    flexBasis: "10%"
   },
   pagination: {
     display: "flex",
@@ -167,36 +135,7 @@ class Index extends React.Component<Props> {
                       </div>
                       <div className={this.props.classes.itemContainer}>
                         {data.jotts_post_by_tag.map(e => (
-                          <Card className={this.props.classes.item} key={e.id}>
-                            <Link href={"/post?slug=" + e.slug} as={"/post/" + e.slug}>
-                              <a>
-                                <Typography variant="h6" className={this.props.classes.itemTitle}>
-                                  {e.title}
-                                </Typography>
-                              </a>
-                            </Link>
-                            <div className={this.props.classes.itemAuthor}>
-                              <Typography color="secondary" className={this.props.classes.itemAuthorName}>
-                                {e.author.name}
-                              </Typography>
-                              <Typography color="textSecondary" className={this.props.classes.itemAuthorHandle}>
-                                @{e.author.handle}
-                              </Typography>
-                            </div>
-                            <Divider />
-                            <Typography variant="body1" className={this.props.classes.itemContent}>
-                              {e.content}
-                            </Typography>
-                            <Divider />
-                            <div className={this.props.classes.itemTags}>
-                              Tags: {e.tags.map(tag => (
-                                <Chip key={tag.tag} label={tag.tag} />
-                              ))}
-                            </div>
-                            <div className={this.props.classes.itemActions}>
-                              Actions
-                      </div>
-                          </Card>
+                          <PostCard data={e}/>
                         ))}
                       </div>
                       <Pagination className={this.props.classes.pagination}
