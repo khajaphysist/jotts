af27aa6012060188d382c5ee37b118d698238435
Khaja Mohiddin Shaik <khajaphysicsatheart@gmail.com>
2019-05-23 05:01:40 +0900

    move vars to json

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
-const dotenv = require('dotenv');
-const dotenvExpand = require('dotenv-expand');
-dotenvExpand(dotenv.config())
-
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
 
-const isProduction = process.env.NODE_ENV === 'production';
-const { S3_IMAGE_PREFIX, GRAPHQL_ENDPOINT, GRAPHQL_PROXY_PATH } = process.env
+const APP_ENV_VARS = process.env.APP_ENV_VARS;
+const vars = JSON.parse(APP_ENV_VARS||"{}");
+const { S3_IMAGE_PREFIX, GRAPHQL_ENDPOINT, GRAPHQL_PROXY_PATH } = vars
 
 module.exports = withTypescript(withCSS({
     publicRuntimeConfig: {
-        graphqlEndPoint: isProduction ? GRAPHQL_PROXY_PATH : GRAPHQL_ENDPOINT,
+        graphqlEndPoint: process.env.NODE_ENV === 'production' ? GRAPHQL_PROXY_PATH : GRAPHQL_ENDPOINT,
         s3ImagePrefix: S3_IMAGE_PREFIX
     },
     serverRuntimeConfig: {
--- src/server/vars.ts
+++ src/server/vars.ts
@@ -1,8 +1,37 @@
-import * as dotenv from 'dotenv';
-import * as dotenvExpand from 'dotenv-expand';
-import { resolve } from 'path';
+const APP_ENV_VARS = process.env.APP_ENV_VARS;
 
-dotenvExpand(dotenv.config({ path: resolve(__dirname, '..', '..', 'vars.env') }))
+let vars: { [key: string]: string } = {};
+
+const requiredvars = [
+    "PRIVATE_KEY",
+    "PUBLIC_KEY",
+    "S3_ENDPOINT",
+    "S3_BUCKENT_NAME",
+    "S3_ACCESS_KEY_ID",
+    "S3_SECRET_ACCESS_KEY",
+    "HASURA_ADMIN_SECRET",
+    "GRAPHQL_ENDPOINT",
+    "PG_CONNECTION_STRING",
+    "ADMIN_EMAIL",
+    "ADMIN_EMAIL_PASS"
+] as const
+
+if (!APP_ENV_VARS) {
+    console.log("NO APP_ENV_VARS is defined")
+    process.exit(1)
+} else {
+    try {
+        vars = JSON.parse(APP_ENV_VARS);
+        const missingvars = requiredvars.filter(key=>!vars[key])
+        if(missingvars.length>0){
+            console.log("MISSING: ", missingvars);
+            process.exit(1)
+        }
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
 
 export const {
     PRIVATE_KEY,
@@ -16,4 +45,4 @@ export const {
     PG_CONNECTION_STRING,
     ADMIN_EMAIL,
     ADMIN_EMAIL_PASS,
-} = process.env as { [key: string]: string }
+} = vars
