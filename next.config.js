d4104f8e16f2352c40b4c7dee522b994279ce01d
Shaik Khaja Mohiddin <shaik_k@worksap.co.jp>
2019-05-22 17:01:57 +0900

    move s3 image prefix to runtime config

--- next.config.js
+++ next.config.js
@@ -9,11 +9,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 const { S3_IMAGE_PREFIX, GRAPHQL_ENDPOINT, GRAPHQL_PROXY_PATH } = process.env
 
 module.exports = withTypescript(withCSS({
-    env: {
-        s3ImagePrefix: S3_IMAGE_PREFIX
-    },
     publicRuntimeConfig: {
-        graphqlEndPoint: isProduction ? GRAPHQL_PROXY_PATH : GRAPHQL_ENDPOINT
+        graphqlEndPoint: isProduction ? GRAPHQL_PROXY_PATH : GRAPHQL_ENDPOINT,
+        s3ImagePrefix: S3_IMAGE_PREFIX
     },
     serverRuntimeConfig: {
         graphqlEndPoint: GRAPHQL_ENDPOINT
--- src/common/vars.ts
+++ src/common/vars.ts
@@ -2,5 +2,5 @@ import getConfig from 'next/config';
 
 const { publicRuntimeConfig, serverRuntimeConfig } = getConfig()
 
-export const s3ImagePrefix = process.env.s3ImagePrefix;
+export const s3ImagePrefix = publicRuntimeConfig.s3ImagePrefix;
 export const graphqlEndpoint: string = process.browser ? publicRuntimeConfig.graphqlEndPoint : serverRuntimeConfig.graphqlEndPoint;
