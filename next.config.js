67ca20f203e65feb47519dce6a768224ebbc3043
Shaik Khaja Mohiddin <shaik_k@worksap.co.jp>
2019-05-22 15:21:15 +0900

    centralize all env variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
+
+dotenvExpand(dotenv.config())
+
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
-module.exports = withTypescript(withCSS())
+
+const isProduction = process.env.NODE_ENV === 'production';
+const { S3_IMAGE_PREFIX, GRAPHQL_ENDPOINT, GRAPHQL_PROXY_PATH } = process.env
+
+module.exports = withTypescript(withCSS({
+    env: {
+        s3ImagePrefix: S3_IMAGE_PREFIX
+    },
+    publicRuntimeConfig: {
+        graphqlEndPoint: isProduction ? GRAPHQL_PROXY_PATH : GRAPHQL_ENDPOINT
+    },
+    serverRuntimeConfig: {
+        graphqlEndPoint: GRAPHQL_ENDPOINT
+    }
+}))
--- src/common/vars.ts
+++ src/common/vars.ts
@@ -1,2 +1,6 @@
-export const s3ImagesUrl = 'http://127.0.0.1:8888/images';
-export const graphqlEndpoint = process.browser ? '/v1/graphql' : process.env.GRAPHQL_ENDPOINT;
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig, serverRuntimeConfig } = getConfig()
+
+export const s3ImagePrefix = process.env.s3ImagePrefix;
+export const graphqlEndpoint: string = process.browser ? publicRuntimeConfig.graphqlEndPoint : serverRuntimeConfig.graphqlEndPoint;
